814ce961465af0a8c07a435628a7c2eb62f79c6a
Gabriela Vilela <gabrielathalitasv@gmail.com>
2020-01-09 18:05:36 -0300

    Cont JS

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Fui carregado de um arquivbo externo");
+
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#primeiropaciente");
+
+console.log(paciente);
